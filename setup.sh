b627fa83d8cb052723ce5361cbead1eabdb738f2
Kei Tsuchiya <kei.tsuchiya86@gmail.com>
2015-04-18 14:20:58 +0900

    refactor setup script

--- setup.sh
+++ setup.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
 
-rm -f ~/.gemrc
-rm -f ~/.gitconfig
-rm -f ~/.screenrc
-rm -f ~/.tmux.conf
-rm -f ~/.tmux-powerlinerc
-rm -f ~/.zshrc
-rm -f ~/.vim
-rm -f ~/.vimrc
-rm -f ~/.vimshrc
-rm -rf ~/dotfiles/tmux/tmux-powerline
+for rc in gemrc gitconfig screenrc tmux.conf tmux-powerlinerc zshrc vim vimrc
+do
+  rm -f ~/.$rc && ln -s ~/dotfiles/$rc ~/.$rc
+done
 
-ln -s ~/dotfiles/gemrc            ~/.gemrc
-ln -s ~/dotfiles/gitconfig        ~/.gitconfig
-ln -s ~/dotfiles/screenrc         ~/.screenrc
-ln -s ~/dotfiles/tmux.conf        ~/.tmux.conf
-ln -s ~/dotfiles/tmux-powerlinerc ~/.tmux-powerlinerc
-ln -s ~/dotfiles/zshrc            ~/.zshrc
-ln -s ~/dotfiles/vim              ~/.vim
-ln -s ~/dotfiles/vimrc            ~/.vimrc
-ln -s ~/dotfiles/zshrc.alias      ~/.vimshrc
+rm -f ~/.vimshrc && ln -s ~/dotfiles/zshrc.alias ~/.vimshrc
+
+rm -rf ~/dotfiles/tmux/tmux-powerline
 git clone https://github.com/erikw/tmux-powerline ~/dotfiles/tmux/tmux-powerline
 ln -s ~/dotfiles/tmux/my-tmux-powerline/themes/mytheme.sh ~/dotfiles/tmux/tmux-powerline/themes/mytheme.sh
 patch -u ~/dotfiles/tmux/tmux-powerline/segments/vcs_branch.sh < ~/dotfiles/tmux/my-tmux-powerline/segments/vcs_branch.patch
